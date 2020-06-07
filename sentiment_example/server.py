3dc8aca9f5aea921f214ae91f517efff47877d8a
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-06-07 11:54:05 -0400

    Add CORS to example apps

--- sentiment_example/server.py
+++ sentiment_example/server.py
@@ -1,8 +1,12 @@
 import os
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 from system import *
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 system = System()
 
 @app.route("/predict", methods=['POST'])
