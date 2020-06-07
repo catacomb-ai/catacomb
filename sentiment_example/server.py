c1c1a9c6793fd816946c62bdd604c178e715ecfb
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-06-07 11:31:47 -0400

    Add jsonify to server response

--- sentiment_example/server.py
+++ sentiment_example/server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from system import *
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ system = System()
 @app.route("/predict", methods=['POST'])
 def predict():
     input_object = request.get_json()['input']
-    return {'output': system.output(input_object)}
+    return jsonify(output=system.output(input_object))
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=int(os.getenv('PORT', 5000)), debug=True)
