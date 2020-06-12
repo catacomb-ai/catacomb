49f7d82633f631540403f4ddc2a2696138cd82ad
mukund-v <mukundv7@gmail.com>
2020-06-11 21:27:15 -0500

    Changed the sentiment model

--- huggingface/sentiment/system.py
+++ huggingface/sentiment/system.py
@@ -1,20 +1,14 @@
 """1) Include/define any dependencies for catacomb.System class"""
-from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+from transformers import pipeline
 import catacomb
-
-MODEL_LOCATION = "mrm8488/distilroberta-base-finetuned-sentiment"
 
 
 """2) Implementing catacomb.System class with initialization and output methods"""
 class System(catacomb.System):
     def __init__(self):
-        tokenizer = AutoTokenizer.from_pretrained(MODEL_LOCATION)
-        model = AutoModelForSequenceClassification.from_pretrained(MODEL_LOCATION)
 
         self.sentiment_pipeline = pipeline(
-            "sentiment-analysis",
-            model=model,
-            tokenizer=tokenizer
+            "sentiment-analysis"
         )
 
     def output(self, sentence):
