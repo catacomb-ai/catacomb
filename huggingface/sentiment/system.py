ac2ce4b1a68cf3d871a151cf5c764c45c89ffbae
mukund-v <mukundv7@gmail.com>
2020-06-11 21:48:07 -0500

    Changed model to DistilBERT

--- huggingface/sentiment/system.py
+++ huggingface/sentiment/system.py
@@ -1,14 +1,19 @@
 """1) Include/define any dependencies for catacomb.System class"""
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 import catacomb
 
+MODEL_NAME = "distilbert-base-uncased-finetuned-sst-2-english"
 
 """2) Implementing catacomb.System class with initialization and output methods"""
 class System(catacomb.System):
     def __init__(self):
+        tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+        model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
 
         self.sentiment_pipeline = pipeline(
-            "sentiment-analysis"
+            "sentiment-analysis",
+            model=model,
+            tokenizer=tokenizer
         )
 
     def output(self, sentence):
