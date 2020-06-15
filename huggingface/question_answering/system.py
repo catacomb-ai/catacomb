968a8c95a85676d3bfd500f2b23e23fcb96402ca
mukund-v <mukundv7@gmail.com>
2020-06-14 20:24:57 -0500

    QA Model init commit

---
+++ huggingface/question_answering/system.py
@@ -0,0 +1,21 @@
+"""1) Include/define any dependencies for catacomb.System class"""
+from transformers import pipeline, AutoTokenizer, AutoModelForQuestionAnswering
+import catacomb
+
+MODEL_NAME = "distilbert-base-uncased-distilled-squad"
+
+"""2) Implementing catacomb.System class with initialization and output methods"""
+class System(catacomb.System):
+    def __init__(self):
+        tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+        model = AutoModelForQuestionAnswering.from_pretrained(MODEL_NAME)
+
+        self.qa_pipeline = pipeline(
+            'question-answering',
+            model=model,
+            tokenizer=tokenizer
+        )
+
+    def output(self, input):
+        context, question = input['context'], input['question']
+        return self.qa_pipeline(question=question, context=context)
